87c2be2000722d7a1f8b70062c3bc379ad4c2802
azhardev631 <azharmehmood74600@gmail.com>
2023-07-03 12:18:38 +0500

    login form with session added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,17 +3,18 @@
     <div class="container custom-login">
         <div class="row">
             <div class="col-sm-4 offset-sm-4">
-                <form>
+                <form method="POST" action="login">
+                    @csrf
                     <div class="row mb-3">
                       <label for="inputEmail3" class=" col-form-label">Email</label>
                       <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3">
+                        <input type="email" name="email" class="form-control" id="inputEmail3">
                       </div>
                     </div>
                     <div class="row mb-3">
                       <label for="inputPassword3" class=" col-form-label">Password</label>
                       <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3">
+                        <input type="password" name="password" class="form-control" id="inputPassword3">
                       </div>
                     </div>
                     <button type="submit" class="btn btn-primary">Sign in</button>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::view('/', 'Login');
+Route::view('/login', 'Login');
+Route::post('/login',[UserController::class,'index']);
+Route::get('/',[ProductController::class,'product']);
